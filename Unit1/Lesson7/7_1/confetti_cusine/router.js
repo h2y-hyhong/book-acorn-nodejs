5fbcf011c12c905952cdcac408b010cdba22caa9
h2y <maya0301@naver.com>
2020-09-04 17:07:51 +0900

    2020-09-04

---
+++ Unit1/Lesson7/7_1/confetti_cusine/router.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const httpStatus = require('http-status-codes'),
+    contentType = require('./content-types'),
+    utils = require('./utils');
+
+const routes = {
+    'GET': {},
+    'POST': {}
+}
+
+exports.handle = (req, res) => {
+    try {
+        routes[req.method][req.url](req, res);
+    } catch(e) {
+        res.writeHead(httpStatus.OK, contentType.html);
+        utils.getFile('viewws/error.html', res);
+    }
+}
+
+exports.get = (url, action) => {
+    routes['GET'][url] = action;
+}
+
+exports.post = (url, action) => {
+    routes['POST'][url] = action;
+}
+
+exports.getRouters = () => routes;
